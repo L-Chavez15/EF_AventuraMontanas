b8de508ea462b725e0555ef0b1851b2f8af390ea
Leonardo Chávez <n00449821pn.pe>
2026-07-01 12:37:58 -0500

    creacion de logica INGRSAR en lista smple de aristas

--- Clases/ListaAristas.cs
+++ Clases/ListaAristas.cs
@@ -8,5 +8,27 @@ namespace Clases
 {
     public class ListaAristas
     {
+        public Arista primero = null;
+
+        public void Insertar(Vertice v, float p)
+        {
+            Arista nuevo = new Arista();
+            nuevo.destino = v;
+            nuevo.peso = p;
+
+            if (primero == null)
+            {
+                primero = nuevo;
+            }
+            else
+            {
+                Arista temp= primero;
+                while (temp.sig != null)
+                {
+                    temp = temp.sig;
+                }
+                temp.sig = nuevo;
+            }
+        }
     }
 }
