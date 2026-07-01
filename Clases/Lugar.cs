ea527cc07247ea2dddad743de09e6bcc1086ff5c
Leonardo Chávez <n00449821pn.pe>
2026-07-01 12:32:55 -0500

    creacion de clase lugar

---
+++ Clases/Lugar.cs
@@ -0,0 +1,15 @@
+﻿namespace Clases
+{
+    public class Lugar
+    {
+        //punto de control
+        public string nombre;
+        public string clima;
+        public string temperatura;
+
+        public override string ToString()
+        {
+            return $"{nombre} - CLIMA: {clima}, TEMPERATURA: {temperatura}";
+        }
+    }
+}
